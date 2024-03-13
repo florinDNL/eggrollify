29b9b101a5115fe8e21be08761d3715225da7b50
florinDNL <dalianflorin@gmail.com>
2024-03-13 18:31:42 +0200

    add break statement to directory search

--- src/eggrollify.py
+++ src/eggrollify.py
@@ -33,6 +33,7 @@ steamDirs = [
 for dir in steamDirs:
     if os.path.exists(os.path.join(HOME, dir)):
         steamDir = os.path.join(HOME, dir)
+    break
 
 if not os.path.exists(TEMP_FOLDER):
     os.makedirs(TEMP_FOLDER)
